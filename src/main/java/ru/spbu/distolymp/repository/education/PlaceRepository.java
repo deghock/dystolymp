5ad4f0a6ebc884dcc6d70739ea4538a3a164f174
daria_usova <ds.usova@inbox.ru>
2021-03-10 00:29:42 +0300

    Add place repository

---
+++ src/main/java/ru/spbu/distolymp/repository/education/PlaceRepository.java
@@ -0,0 +1,24 @@
+package ru.spbu.distolymp.repository.education;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import ru.spbu.distolymp.entity.education.Place;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Daria Usova
+ */
+public interface PlaceRepository extends CrudRepository<Place, Long> {
+
+    List<Place> findAllByDivisionIdOrderByOrder(Long id);
+
+    @Query("select coalesce(max(p.order), 1) from Place p where p.division.id=?1")
+    Integer findMaxOrderByDivisionId(Long id);
+
+    Optional<Place> findByDivisionIdAndOrder(Long divisionId, Integer order);
+
+    void deletePlacesByIdInAndDivisionId(List<Long> idList, Long divisionId);
+
+}
