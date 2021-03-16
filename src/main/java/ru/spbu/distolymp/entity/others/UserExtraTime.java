7626ef03c6e1725ddf3cc032dc8d9a22c30d42ae
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-03-16 17:01:08 +0300

    Create UserExtraTime Entity class

---
+++ src/main/java/ru/spbu/distolymp/entity/others/UserExtraTime.java
@@ -0,0 +1,57 @@
+package ru.spbu.distolymp.entity.others;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.Type;
+import ru.spbu.distolymp.entity.lists.List;
+import ru.spbu.distolymp.entity.users.Staff;
+import ru.spbu.distolymp.entity.users.User;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+/**
+ * @author Vladislav Konovalov
+ */
+@Data
+@EqualsAndHashCode(of = {"id"})
+@Entity
+@Table(name = "user_extra_time")
+public class UserExtraTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_extra")
+    private Long id;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "extra_date")
+    private Date extraDate;
+
+    @NotNull
+    @Temporal(TemporalType.TIME)
+    @Column(name = "period")
+    private Date period;
+
+    @Type(type = "text")
+    @Column(name = "reason")
+    private String reason;
+
+    @NotNull
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_user", referencedColumnName = "id_user")
+    private User user;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_list", referencedColumnName = "id_list")
+    private List list;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_staff", referencedColumnName = "id_staff")
+    private Staff staff;
+
+}
