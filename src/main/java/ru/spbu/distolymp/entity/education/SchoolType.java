c6a04502ebb4f15849b7d3f92d52188b107612eb
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-03-16 17:01:34 +0300

    Create SchoolType Entity class

---
+++ src/main/java/ru/spbu/distolymp/entity/education/SchoolType.java
@@ -0,0 +1,35 @@
+package ru.spbu.distolymp.entity.education;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import ru.spbu.distolymp.entity.enumeration.Visible;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Vladislav Konovalov
+ */
+@Data
+@EqualsAndHashCode(of = {"id"})
+@Entity
+@Table(name = "school_types")
+public class SchoolType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_type")
+    private Long id;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "visible", columnDefinition = "ENUM('yes', 'no')", nullable = false)
+    private Visible visible;
+
+}
