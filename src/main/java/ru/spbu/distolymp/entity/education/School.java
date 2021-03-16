0189811109eb1bb892799c08e48a62f455268883
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-03-16 17:01:52 +0300

    Create School Entity class

---
+++ src/main/java/ru/spbu/distolymp/entity/education/School.java
@@ -0,0 +1,66 @@
+package ru.spbu.distolymp.entity.education;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.Type;
+import ru.spbu.distolymp.entity.enumeration.Visible;
+import ru.spbu.distolymp.entity.lists.Division;
+import ru.spbu.distolymp.entity.geography.Town;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Vladislav Konovalov
+ */
+@Data
+@EqualsAndHashCode(of = {"id"})
+@Entity
+@Table(name = "schools")
+public class School {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_school")
+    private Long id;
+
+    @NotNull
+    @Size(max = 11)
+    @Column(name = "number")
+    private Integer number;
+
+    @NotNull
+    @Size(max = 255)
+    @Column(name = "title")
+    private String title;
+
+    @NotNull
+    @Type(type = "text")
+    @Column(name = "full_name")
+    private String address;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "visible", columnDefinition = "ENUM('yes', 'no')", nullable = false)
+    private Visible visible;
+
+    @NotNull
+    @Column(name = "editing", columnDefinition = "TINYINT")
+    private boolean editing;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "id_division", referencedColumnName = "id_division")
+    private Division division;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_town", referencedColumnName = "id_town")
+    private Town town;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_type", referencedColumnName = "id_type")
+    private SchoolType schoolType;
+
+}
