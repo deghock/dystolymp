116abd603b7ac8562db370a3dbce57eb620be94b
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-03-22 16:36:20 +0300

    Added delete method

--- src/main/java/ru/spbu/distolymp/controller/admin/directories/grades/GradeController.java
+++ src/main/java/ru/spbu/distolymp/controller/admin/directories/grades/GradeController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import ru.spbu.distolymp.service.admin.directories.grades.api.GradeService;
@@ -19,6 +20,7 @@ public class GradeController {
     private static final String ROOT_DIR = "admin/directories/grades/";
     private static final String LIST_PAGE = ROOT_DIR + "list";
     private static final String ENTRY_REDIRECT_PAGE = "redirect:/division/entry";
+    private static final String REDIRECT_GRADE_LIST = "redirect:/grades/list";
 
     private final GradeService gradeService;
 
@@ -29,6 +31,14 @@ public class GradeController {
         if (divisionId == null) return ENTRY_REDIRECT_PAGE;
         gradeService.fillShowAllGradesModelMap(modelMap, divisionId);
         return LIST_PAGE;
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteGrade(@PathVariable("id") Long id,
+                              @SessionAttribute(value = "idDivision", required = false)
+                                      Long divisionId) {
+        gradeService.deleteGradeById(id, divisionId);
+        return REDIRECT_GRADE_LIST;
     }
 
 }
