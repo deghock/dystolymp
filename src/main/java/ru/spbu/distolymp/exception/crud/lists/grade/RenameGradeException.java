dab93cd328fae19befe4d1710755185f9a94922a
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-03-22 21:15:44 +0300

    Created RenameGradeException

---
+++ src/main/java/ru/spbu/distolymp/exception/crud/lists/grade/RenameGradeException.java
@@ -0,0 +1,15 @@
+package ru.spbu.distolymp.exception.crud.lists.grade;
+
+/**
+ * @author Vladislav Konovalov
+ */
+public class RenameGradeException extends RuntimeException {
+
+    public RenameGradeException() {
+    }
+
+    public RenameGradeException(String message) {
+        super(message);
+    }
+
+}
