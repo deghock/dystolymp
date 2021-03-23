1d3900e722653fc765f950670c4b5d7240acdd51
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-03-23 19:30:43 +0300

    Added findByIdAndDivisionId method

--- src/main/java/ru/spbu/distolymp/repository/lists/GradeRepository.java
+++ src/main/java/ru/spbu/distolymp/repository/lists/GradeRepository.java
@@ -14,4 +14,6 @@ public interface GradeRepository extends CrudRepository<Grade, Long> {
 
     void deleteGradeByIdAndDivisionId(Long id, Long divisionId);
 
+    Grade findByIdAndDivisionId(Long id, Long divisionId);
+
 }
