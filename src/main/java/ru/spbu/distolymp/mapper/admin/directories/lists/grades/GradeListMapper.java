46d8726e9c75fd85b83ee4e45dec20ced79eaa77
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-03-23 19:31:26 +0300

    Removed useless fields and annotations

--- src/main/java/ru/spbu/distolymp/mapper/admin/directories/lists/grades/GradeListMapper.java
+++ src/main/java/ru/spbu/distolymp/mapper/admin/directories/lists/grades/GradeListMapper.java
@@ -4,7 +4,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import ru.spbu.distolymp.dto.admin.directories.lists.grades.GradeListDto;
 import ru.spbu.distolymp.entity.lists.Grade;
-import ru.spbu.distolymp.service.crud.api.lists.DivisionCrudService;
 import ru.spbu.distolymp.service.crud.api.lists.ListingCrudService;
 import ru.spbu.distolymp.service.crud.api.users.StaffCrudService;
 
@@ -14,19 +13,17 @@ import java.util.List;
 /**
  * @author Vladislav Konovalov
  */
-@Mapper(uses = {DivisionCrudService.class, StaffCrudService.class, ListingCrudService.class})
+@Mapper(uses = {StaffCrudService.class, ListingCrudService.class})
 public interface GradeListMapper {
 
     List<GradeListDto> toDtoList(List<Grade> grades);
 
-    @Mapping(target = "divisionId", source = "division.id")
     @Mapping(target = "staffId", source = "staff.id")
     @Mapping(target = "staffLogin", source = "staff.login")
     @Mapping(target = "listingId", source = "listing.id")
     @Mapping(target = "listingName", source = "listing.name")
     GradeListDto toDto(Grade grade);
 
-    @Mapping(target = "division", source = "divisionId")
     @Mapping(target = "staff", source = "staffId")
     @Mapping(target = "listing", source = "listingId")
     Grade toEntity(GradeListDto gradeListDto);
