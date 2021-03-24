342bad03e2067e7dc7712db44b106d4292d3c9c7
Maxim <mackandreev2001@mail.ru>
2021-03-24 14:45:46 +0300

    Update institutes & places controllers
    Add "maxOrder" attribute to modelMap

--- src/main/java/ru/spbu/distolymp/controller/admin/directories/places/PlaceController.java
+++ src/main/java/ru/spbu/distolymp/controller/admin/directories/places/PlaceController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.spbu.distolymp.dto.entity.education.PlaceDto;
 import ru.spbu.distolymp.exception.admin.directories.places.PlaceServiceException;
 import ru.spbu.distolymp.exception.crud.education.PlaceCrudServiceException;
+import ru.spbu.distolymp.repository.education.PlaceRepository;
 import ru.spbu.distolymp.service.admin.directories.places.api.PlaceService;
 
 import javax.validation.Valid;
@@ -30,6 +31,7 @@ public class PlaceController {
     private static final String LIST_REDIRECT_PAGE = "redirect:/places/list";
 
     private final PlaceService placeService;
+    private final PlaceRepository placeRepository;
 
     @GetMapping("/list")
     public String showAllPlaces(ModelMap modelMap,
@@ -47,8 +49,8 @@ public class PlaceController {
         if (id == null) {
             return ENTRY_REDIRECT_PAGE;
         }
-
         modelMap.put("place", placeService.getNewPlaceDto(id));
+        modelMap.put("maxOrder", placeRepository.findMaxOrderByDivisionId(id)+1);
         return EDIT_PAGE;
     }
 
@@ -57,7 +59,6 @@ public class PlaceController {
         if (bindingResult.hasErrors()) {
             return EDIT_PAGE;
         }
-
         placeService.saveOrEditPlace(placeDto);
         return LIST_REDIRECT_PAGE;
     }
@@ -65,6 +66,8 @@ public class PlaceController {
     @GetMapping("/edit/{id}")
     public String showEditPage(@PathVariable("id") Long id, ModelMap modelMap) {
         modelMap.put("place", placeService.getPlaceDtoById(id));
+        Long divId = placeRepository.findById(id).get().getDivision().getId();
+        modelMap.put("maxOrder", placeRepository.findMaxOrderByDivisionId(divId));
         return EDIT_PAGE;
     }
 
