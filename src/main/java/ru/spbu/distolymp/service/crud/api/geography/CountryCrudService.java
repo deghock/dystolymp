ee100cbfc1df7bdac9dba08ec811d63d842c10b9
daria_usova <ds.usova@inbox.ru>
2021-03-27 10:34:26 +0300

    Add country crud service

---
+++ src/main/java/ru/spbu/distolymp/service/crud/api/geography/CountryCrudService.java
@@ -0,0 +1,34 @@
+package ru.spbu.distolymp.service.crud.api.geography;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import ru.spbu.distolymp.dto.entity.geography.country.CountryDto;
+import ru.spbu.distolymp.entity.geography.Country;
+
+import java.util.List;
+
+/**
+ * @author Daria Usova
+ */
+public interface CountryCrudService {
+
+    void save(CountryDto countryDto);
+
+    CountryDto getCountryByIdOrNull(Long id);
+
+    List<CountryDto> getCountries(Sort sort);
+
+    List<CountryDto> getCountries(Pageable pageable);
+
+    List<CountryDto> getCountries(Specification<Country> specs, Sort sort);
+
+    List<CountryDto> getCountries(Specification<Country> specs, Pageable pageable);
+
+    void update(CountryDto countryDto);
+
+    void saveOrUpdate(CountryDto countryDto);
+
+    void deleteCountriesByIdIn(List<Long> ids);
+
+}
