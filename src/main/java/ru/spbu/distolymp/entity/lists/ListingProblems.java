439d9c18229cf4d8fc0bfa096e9a025ffe9a1eb2
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-03-30 20:58:46 +0300

    Added ListingProblems Entity

---
+++ src/main/java/ru/spbu/distolymp/entity/lists/ListingProblems.java
@@ -0,0 +1,37 @@
+package ru.spbu.distolymp.entity.lists;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import ru.spbu.distolymp.entity.tasks.ProblemType;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Vladislav Konovalov
+ */
+@Data
+@EqualsAndHashCode(of = {"id"})
+@Entity
+@Table(name = "list_problems")
+public class ListingProblems {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_lp")
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_list", referencedColumnName = "id_list")
+    private Listing listing;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_problem", referencedColumnName = "id_problem")
+    private ProblemType problem;
+
+    @Column(name = "`order`")
+    private Integer order;
+
+}
