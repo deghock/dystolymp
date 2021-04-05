e7c174c450c24b855126f1b36068e2313bca1e38
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-04-05 15:48:09 +0300

    Added findAllBy methods

--- src/main/java/ru/spbu/distolymp/repository/geography/TownRepository.java
+++ src/main/java/ru/spbu/distolymp/repository/geography/TownRepository.java
@@ -1,5 +1,7 @@
 package ru.spbu.distolymp.repository.geography;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.spbu.distolymp.dto.entity.geography.town.TownNameDto;
 import ru.spbu.distolymp.entity.geography.Town;
@@ -7,10 +9,14 @@ import ru.spbu.distolymp.entity.geography.Town;
 import java.util.List;
 
 /**
- * @author Daria Usova
+ * @author Daria Usova, Vladislav Konovalov
  */
 public interface TownRepository extends PagingAndSortingRepository<Town, Long> {
 
     List<TownNameDto> getTownsByRegionIdOrderByName(Long id);
+
+    List<Town> findAllBy(Pageable pageable);
+
+    List<Town> findAllBy(Sort sort);
 
 }
