aa318c7fd3857fd79ee9bb51e441ad1fb7276e87
daria_usova <ds.usova@inbox.ru>
2021-04-05 20:50:26 +0300

    Update CategoryMapper after deleting divisionId field in CategoryDto

--- src/main/java/ru/spbu/distolymp/mapper/entity/lists/CategoryMapper.java
+++ src/main/java/ru/spbu/distolymp/mapper/entity/lists/CategoryMapper.java
@@ -1,7 +1,6 @@
 package ru.spbu.distolymp.mapper.entity.lists;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import ru.spbu.distolymp.dto.entity.lists.CategoryDto;
 import ru.spbu.distolymp.entity.lists.Category;
 
@@ -13,11 +12,10 @@ import java.util.List;
 @Mapper
 public interface CategoryMapper {
 
-    @Mapping(target = "divisionId", source = "division.id")
     CategoryDto toDto(Category category);
 
-    List<CategoryDto> toDtoList(List<Category> categoryList);
-
     Category toEntity(CategoryDto categoryDto);
+
+    List<CategoryDto> toDtoList(List<Category> categoryList);
 
 }
