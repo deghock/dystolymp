1b53af775e78bd8dd2c0f4b5f61220b9c758c185
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-04-05 11:43:14 +0300

    Removed useless annotation

--- src/main/java/ru/spbu/distolymp/validation/common/validator/EmailValidator.java
+++ src/main/java/ru/spbu/distolymp/validation/common/validator/EmailValidator.java
@@ -1,6 +1,5 @@
 package ru.spbu.distolymp.validation.common.validator;
 
-import lombok.RequiredArgsConstructor;
 import ru.spbu.distolymp.validation.common.annotation.Email;
 
 import javax.validation.ConstraintValidator;
@@ -9,7 +8,6 @@ import javax.validation.ConstraintValidatorContext;
 /**
  * @author Vladislav Konovalov
  */
-@RequiredArgsConstructor
 public class EmailValidator implements ConstraintValidator<Email, String> {
 
     @Override
