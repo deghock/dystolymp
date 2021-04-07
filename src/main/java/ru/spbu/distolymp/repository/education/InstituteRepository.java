822067491b947d092509a0ff42d05a289cf9d5c3
Maxim <mackandreev2001@mail.ru>
2021-04-07 14:29:58 +0300

    Optimize setNewOrder method
    Added new queries that allow to get several instances from repository at once

--- src/main/java/ru/spbu/distolymp/repository/education/InstituteRepository.java
+++ src/main/java/ru/spbu/distolymp/repository/education/InstituteRepository.java
@@ -15,6 +15,8 @@ public interface InstituteRepository extends CrudRepository<Institute, Long> {
     @Query("select coalesce(max(i.order), 0) from Institute i")
     Integer findMaxOrder();
 
+    List<Institute> findByOrderBetween(Integer startOrder, Integer endOrder);
+
     void deleteInstitutesByIdIn(List<Long> idList);
 
     Optional<Institute> findByOrder(Integer order);
