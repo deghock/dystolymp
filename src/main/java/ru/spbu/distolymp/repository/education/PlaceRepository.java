218929bc7c002542b81abb3b7d11f8cbc80dc416
daria_usova <ds.usova@inbox.ru>
2021-04-07 21:54:24 +0300

    Update findMaxOrder in PlaceRepository

--- src/main/java/ru/spbu/distolymp/repository/education/PlaceRepository.java
+++ src/main/java/ru/spbu/distolymp/repository/education/PlaceRepository.java
@@ -13,8 +13,8 @@ public interface PlaceRepository extends CrudRepository<Place, Long> {
 
     List<Place> findAllByOrderByOrder();
 
-    @Query("select coalesce(max(p.order), 0) from Place p where p.division.id=?1")
-    Integer findMaxOrderByDivisionId(Long id);
+    @Query("select coalesce(max(p.order), 0) from Place p")
+    Integer findMaxOrder();
 
     List<Place> findByDivisionIdAndOrderBetween(Long divisionId, Integer startOrder, Integer endOrder);
 
