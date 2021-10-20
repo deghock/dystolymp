ba156ded6fdefb73d41d259d6d4770b51a284cf6
Maxim <mackandreev2001@mail.ru>
2021-10-20 22:10:09 +0300

    Create schoolFilter & schoolSpecs

---
+++ src/main/java/ru/spbu/distolymp/dto/admin/directories/schools/SchoolFilter.java
@@ -0,0 +1,19 @@
+package ru.spbu.distolymp.dto.admin.directories.schools;
+
+import lombok.Data;
+
+/**
+ * @author Maxim Andreev
+ */
+@Data
+public class SchoolFilter {
+    private String title;
+    private Integer number;
+    private Long countryId;
+    private Long regionId;
+    private Long townId;
+    //private Long districtId;
+    //private int resultSize;
+    //private boolean showVisible;
+    //private boolean showHidden;
+}
