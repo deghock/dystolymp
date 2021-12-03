860229efdb5c71c728a23b48688fa2bc256646a7
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-12-03 14:09:10 +0300

    Added getDetails method

--- src/main/java/ru/spbu/distolymp/controller/admin/directories/towns/TownController.java
+++ src/main/java/ru/spbu/distolymp/controller/admin/directories/towns/TownController.java
@@ -3,8 +3,11 @@ package ru.spbu.distolymp.controller.admin.directories.towns;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.spbu.distolymp.exception.crud.geography.TownCrudServiceException;
 import ru.spbu.distolymp.service.admin.directories.towns.api.TownService;
 
 /**
@@ -14,11 +17,11 @@ import ru.spbu.distolymp.service.admin.directories.towns.api.TownService;
 @RequiredArgsConstructor
 @RequestMapping("/towns")
 public class TownController {
-
     private static final int DEFAULT_RESULT_SIZE = 20;
     private static final String ROOT_DIR = "admin/directories/towns/";
     private static final String LIST_PAGE = ROOT_DIR + "list";
-
+    private static final String DETAILS_PAGE = ROOT_DIR + "details";
+    private static final String PAGE_404 = "exception/404";
     private final TownService townService;
 
     @GetMapping("/list")
@@ -27,4 +30,14 @@ public class TownController {
         return LIST_PAGE;
     }
 
+    @GetMapping("/details/{id}")
+    public String getDetails(ModelMap modelMap, @PathVariable("id") Long townId) {
+        townService.fillShowTownDetailsModelMap(modelMap, townId);
+        return DETAILS_PAGE;
+    }
+
+    @ExceptionHandler(TownCrudServiceException.class)
+    public String handleTownCrudServiceException() {
+        return PAGE_404;
+    }
 }
