9b74aee7b15b49286046670738768ea838ddcabf
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-12-05 19:13:43 +0300

    Refactor DTO to be applicable for editing

--- src/main/java/ru/spbu/distolymp/dto/admin/directories/towns/TownDetailsDto.java
+++ src/main/java/ru/spbu/distolymp/dto/admin/directories/towns/TownDetailsDto.java
@@ -3,6 +3,8 @@ package ru.spbu.distolymp.dto.admin.directories.towns;
 import lombok.Data;
 import ru.spbu.distolymp.dto.entity.education.school.SchoolNumberTitleDto;
 import ru.spbu.distolymp.dto.entity.geography.region.RegionNameCodeCountryDto;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 /**
@@ -11,9 +13,12 @@ import java.util.List;
 @Data
 public class TownDetailsDto {
     Long id;
+    @NotNull(message = "{town.name.required}")
+    @Size(max = 255, message = "{town.name.tooLong}")
     String name;
     List<SchoolNumberTitleDto> schools;
     RegionNameCodeCountryDto region;
+    Long countryId;
     boolean visible;
     boolean editable;
 }
