322821dc27248d152e65e3af561cda20ad0a27fa
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-12-05 19:12:10 +0300

    Added edit method

--- src/main/java/ru/spbu/distolymp/controller/admin/directories/towns/TownController.java
+++ src/main/java/ru/spbu/distolymp/controller/admin/directories/towns/TownController.java
@@ -22,6 +22,7 @@ public class TownController {
     private static final String LIST_PAGE = ROOT_DIR + "list";
     private static final String DETAILS_PAGE = ROOT_DIR + "details";
     private static final String PAGE_404 = "exception/404";
+    private static final String EDIT_PAGE = ROOT_DIR + "edit";
     private final TownService townService;
 
     @GetMapping("/list")
@@ -34,6 +35,12 @@ public class TownController {
     public String getDetails(ModelMap modelMap, @PathVariable("id") Long townId) {
         townService.fillShowTownDetailsModelMap(modelMap, townId);
         return DETAILS_PAGE;
+    }
+
+    @GetMapping("/edit/{id}")
+    public String getEditPage(ModelMap modelMap, @PathVariable("id") Long townId) {
+        townService.fillShowEditPageModelMap(modelMap, townId);
+        return EDIT_PAGE;
     }
 
     @ExceptionHandler(TownCrudServiceException.class)
