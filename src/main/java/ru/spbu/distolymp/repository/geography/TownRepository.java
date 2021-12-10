cba1096d8f38ba4bdb26e8a809104aec1cafffa7
Vladislav Konovalov <konovalov.phys@gmail.com>
2021-12-10 20:41:09 +0300

    Added JpaSpecificationExecutor extension

--- src/main/java/ru/spbu/distolymp/repository/geography/TownRepository.java
+++ src/main/java/ru/spbu/distolymp/repository/geography/TownRepository.java
@@ -2,6 +2,7 @@ package ru.spbu.distolymp.repository.geography;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.spbu.distolymp.dto.entity.geography.town.TownNameDto;
 import ru.spbu.distolymp.entity.geography.Town;
@@ -10,7 +11,7 @@ import java.util.List;
 /**
  * @author Daria Usova, Vladislav Konovalov
  */
-public interface TownRepository extends PagingAndSortingRepository<Town, Long> {
+public interface TownRepository extends PagingAndSortingRepository<Town, Long>, JpaSpecificationExecutor<Town> {
     List<TownNameDto> getTownsByRegionIdOrderByName(Long id);
     List<Town> findAllBy(Pageable pageable);
     List<Town> findAllBy(Sort sort);
