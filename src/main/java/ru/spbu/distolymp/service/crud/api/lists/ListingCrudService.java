f62e2297f3bb373901ab4952f30297674ec4ba22
Vladislav Konovalov <konovalov.phys@gmail.com>
2022-01-18 22:34:41 +0300

    Bug fix: null staff or null listing led to an exception

--- src/main/java/ru/spbu/distolymp/service/crud/api/lists/ListingCrudService.java
+++ src/main/java/ru/spbu/distolymp/service/crud/api/lists/ListingCrudService.java
@@ -2,16 +2,12 @@ package ru.spbu.distolymp.service.crud.api.lists;
 
 import ru.spbu.distolymp.dto.entity.lists.listing.ListingNameDto;
 import ru.spbu.distolymp.entity.lists.Listing;
-
 import java.util.List;
 
 /**
  * @author Vladislav Konovalov
  */
 public interface ListingCrudService {
-
-    Listing getListingById(Long id);
-
+    Listing getListingByIdOrNull(Long id);
     List<ListingNameDto> getAllListings();
-
 }
